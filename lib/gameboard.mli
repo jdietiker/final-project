1eb9eea2c07cdfaf562ca8ccf86a1d12c64dd08d
lduesterwald <leduesterwald@gmail.com>
2024-03-24 18:56:43 -0400

    Added function to initialize gameboard.

--- lib/gameboard.mli
+++ lib/gameboard.mli
@@ -1,2 +1,11 @@
 type elt
+(** [elt] is the type of cells in the board 2d array. Each elt contains information about the kind of multiplier in the space and the letter played (or ' ' if no letter is played)*)
+
 type t = elt array array
+(** [t] is the type of Gameboard which is represented as a 2D array of elements of type [elt]. The jth element of the ith sub-array is the jth column and the ith row *)
+
+val empty : t
+(** [empty] is an empty board. *)
+
+val init : t
+(** [init] is the gameboard inialized to its 15x15 starting state with multiplers and no letters played. *)

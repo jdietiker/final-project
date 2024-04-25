27e2faf6c1d36f5af9160ac424691ba9b4002b73
bkn2545@gmail.com <bkn2545@gmail.com>
2024-04-25 16:38:05 -0400

    Implemented word check
    -not tested

--- lib/scrabbl.ml
+++ lib/scrabbl.ml
@@ -1,3 +1,12 @@
 let dict = Arg.read_arg "data/dictionary.txt"
-let valid_word str = failwith "TODO"
+
+module StringSet = Set.Make (struct
+  type t = string
+
+  let compare a b =
+    compare (String.lowercase_ascii a) (String.lowercase_ascii b)
+end)
+
+let dict = StringSet.of_list (Array.to_list dict)
+let valid_word str = StringSet.mem str dict
 let grade_word w = failwith "TODO"
